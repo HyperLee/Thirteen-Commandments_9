9f725ca3a4285cb500ea867c3919ea41e9e86a2b
Peter.Chiu <tidub76217@hotmail.com>
2022-02-16 22:01:42 +0800

    Initial Commit

---
+++ Thirteen-Commandments_9/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Thirteen-Commandments_9
+//
+//  Created by 邱字立 on 2022/2/16.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
